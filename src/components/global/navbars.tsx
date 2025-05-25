aa94d78f4176ff9227d8815d8dec4877c911458a
HoangDucBach <hoangbach0985@gmail.com>
2025-05-25 17:42:43 +0700

    feat(components): prepare for full code

---
+++ src/components/global/navbars.tsx
@@ -0,0 +1,67 @@
+"use client"
+
+import { siteConfig } from "@/config/site";
+import { chakra, For, HStack, HtmlProps, Image, Link } from "@chakra-ui/react"
+import NextImage from "next/image";
+const ChakraHeader = chakra.header;
+
+interface LandingNavbarProps extends HtmlProps { }
+export const LandingNavbar: React.FC<LandingNavbarProps> = (props) => {
+    const Brand = () => (
+        <Image asChild>
+            <NextImage
+                src="/RootFaviconLight.png"
+                alt="Brand Logo"
+                width={48}
+                height={48}
+            />
+        </Image>
+    );
+
+    const NavLinks = () => (
+        <HStack flex={"1"} as={"nav"} justify={"space-between"}>
+            <For each={Object.entries(siteConfig.paths)}>
+                {([key, path]) => (
+                    <Link key={key} href={path.href}>{path.label}</Link>
+                )}
+            </For>
+        </HStack>
+    );
+
+    return (
+        <ChakraHeader
+            position="sticky"
+            w={"full"}
+            top={0}
+            left={0}
+            right={0}
+            zIndex={"sticky"}
+            {...props}
+        >
+            <HStack gap={"8"} justify={"space-between"} align={"center"} p={4}>
+                <Brand />
+                <NavLinks />
+            </HStack>
+        </ChakraHeader>
+    );
+}
+
+interface DashboardNavbarProps extends HtmlProps { }
+export const DashboardNavbar: React.FC<DashboardNavbarProps> = (props) => {
+    return (
+        <ChakraHeader
+            as="nav"
+            position="fixed"
+            top={0}
+            left={0}
+            right={0}
+            zIndex={1000}
+            bg="gray.800"
+            color="white"
+            boxShadow="md"
+            {...props}
+        >
+            {/* Navbar content goes here */}
+        </ChakraHeader>
+    );
+}
---
+++ src/config/site.ts
@@ -0,0 +1,18 @@
+export const siteConfig = {
+    name: "Vinaswap",
+    description: "A decentralized exchange (DEX) for trading cryptocurrencies and tokens on the Vina Network.",
+    paths: {
+        home: {
+            href: "/",
+            label: "Trang chủ",
+        },
+        docs: {
+            href: "https://docs.vinaswap.org",
+            label: "Tài liệu",
+        },
+        about: {
+            href: "/about",
+            label: "Về chúng tôi",
+        },
+    }
+}
