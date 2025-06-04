49ccd36d7ec98e7b8b094880d6534dfd4f1a22d4
HoangDucBach <hoangbach0985@gmail.com>
2025-06-04 22:32:02 +0700

    chore(dashboard): test background cover

--- src/app/(dashboard)/provider.tsx
+++ src/app/(dashboard)/provider.tsx
@@ -4,6 +4,8 @@ import { useAppKit } from "@reown/appkit/react";
 import { useEffect } from "react";
 import { useAccount } from "wagmi";
 import { FallbackView } from "./_components/FallbackView";
+import NextImage from "next/image";
+import { Image } from "@chakra-ui/react";
 
 interface ProviderProps extends React.PropsWithChildren { }
 export const Provider: React.FC<ProviderProps> = ({ children }) => {
@@ -20,10 +22,19 @@ export const Provider: React.FC<ProviderProps> = ({ children }) => {
     }, [isConnected]);
 
     if (!isConnected) {
-        return <FallbackView
-            label="Bạn chưa kết nối ví"
-            subtitle="Vui lòng kết nối ví để tiếp tục"
-        />;
+        return <>
+            <Image asChild pos={"absolute"}>
+                <NextImage
+                    src={"/assets/bg-cover-plateau.png"}
+                    alt={"Plateau Background"}
+                    fill
+                />
+            </Image>
+            <FallbackView
+                label="Bạn chưa kết nối ví"
+                subtitle="Vui lòng kết nối ví để tiếp tục"
+            />
+        </>;
     };
 
     return (
