3f95b326ec3ec421eb6ea548da0d165c34cbc41d
HoangDucBach <hoangbach0985@gmail.com>
2025-06-06 05:09:42 +0700

    feat(dashboard): menu profile

--- src/app/(dashboard)/_components/ProfileMenu.tsx
+++ src/app/(dashboard)/_components/ProfileMenu.tsx
@@ -1,16 +1,24 @@
 "use client";
 
-import { AvatarFallback, AvatarImage, AvatarRoot, HStack, Image, Spinner, StackProps, Text } from "@chakra-ui/react";
-import { useAccount, useDisconnect, useEnsAvatar, useEnsName } from "wagmi";
+import { AvatarFallback, AvatarImage, AvatarRoot, For, HStack, Spinner, StackProps, Text } from "@chakra-ui/react";
+import { useDisconnect, useEnsAvatar, useEnsName } from "wagmi";
 import { useAppKit, useAppKitAccount } from "@reown/appkit/react";
 import NextImage from "next/image";
+import { useRouter } from "next/navigation";
 import { useMemo } from "react";
+import { FaUser } from "react-icons/fa";
+
 import { formatAddress } from "@/libs";
 import { ConnectWalletButton } from "@/components/global/wallet";
+import { MenuContent, MenuItem, MenuRoot, MenuTrigger } from "@/components/ui/menu";
+import { motion } from "framer-motion";
+
+const MotionMenuItem = motion.create(MenuItem);
 
 interface Props extends StackProps { }
 export const ProfileMenu: React.FC<Props> = ({ children, ...props }) => {
     const { disconnect } = useDisconnect()
+    const router = useRouter();
     const { open } = useAppKit();
     const { address, isConnected, caipAddress, status, embeddedWalletInfo } = useAppKitAccount();
 
@@ -39,28 +47,72 @@ export const ProfileMenu: React.FC<Props> = ({ children, ...props }) => {
             </AvatarFallback>
         </AvatarRoot>
     );
+
+    const items = [
+        {
+            label: "Profile",
+            icon: <FaUser />,
+            action: () => {
+                router.push(`/profile/${address}`);
+            }
+        },
+        {
+            label: "Wallet",
+            icon: <NextImage src={"/assets/logo-connect-wallet.svg"} alt="Connect Wallet" width={16} height={16} />,
+            action: () => open({
+                view: "Account"
+            })
+        }
+    ]
+
     if (!isConnected && !isConnecting) return <ConnectWalletButton />;
 
     return (
-        <HStack
-            rounded={"full"}
-            p={"2"}
-            bg={"bg.subtle"}
-            shadow={"sm"}
-            cursor={"pointer"}
-            onClick={() => open({
-                view: "Account"
-            })}
-            {...props}
-        >
-            <ProfileAvatar />
-            {isConnecting &&
-                <>
-                    <Text fontSize={"sm"} fontWeight={"semibold"} color={"fg.muted"}>Connecting</Text>
-                    <Spinner color={"fg.muted"} size={"sm"} />
-                </>
-            }
-            <Text fontSize={"sm"} fontWeight={"medium"}>{formatAddress(address)}</Text>
-        </HStack>
+        <MenuRoot>
+            <MenuTrigger>
+                <HStack
+                    rounded={"full"}
+                    p={"2"}
+                    bg={"bg.subtle"}
+                    shadow={"sm"}
+                    cursor={"pointer"}
+                    {...props}
+                >
+                    <ProfileAvatar />
+                    {isConnecting &&
+                        <>
+                            <Text fontSize={"sm"} fontWeight={"semibold"} color={"fg.muted"}>Connecting</Text>
+                            <Spinner color={"fg.muted"} size={"sm"} />
+                        </>
+                    }
+                    <Text fontSize={"sm"} fontWeight={"medium"}>{formatAddress(address)}</Text>
+                </HStack>
+            </MenuTrigger>
+            <MenuContent>
+                <For each={items} >
+                    {(item) => (
+                        <MotionMenuItem
+                            rounded={"lg"}
+                            value={item.label}
+                            key={item.label}
+                            whileHover={{
+                                scale: 1.02,
+                            }}
+                            transition={{
+                                type: "spring",
+                                stiffness: 300,
+                                damping: 20,
+                            }}
+                            onClick={() => {
+                                item.action();
+                            }}
+                        >
+                            {item.icon}
+                            {item.label}
+                        </MotionMenuItem>
+                    )}
+                </For>
+            </MenuContent>
+        </MenuRoot>
     );
 };
