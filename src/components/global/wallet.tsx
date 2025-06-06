ff72e25df8ca82c38941ccd7065665267e03fe65
HoangDucBach <hoangbach0985@gmail.com>
2025-06-07 05:57:59 +0700

    style(kyc): set up auth

--- src/components/global/wallet.tsx
+++ src/components/global/wallet.tsx
@@ -3,12 +3,23 @@
 import { useAppKit, useAppKitAccount, useAppKitState } from "@reown/appkit/react";
 import { Button, ButtonProps } from "../ui/button";
 import { useMemo } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { vinaswapApi } from "../../../services/axios";
 
 interface ConnectWalletButtonProps extends ButtonProps {
 }
 export const ConnectWalletButton: React.FC<ConnectWalletButtonProps> = (props) => {
     const { open, } = useAppKit();
-    const { status } = useAppKitAccount();
+    const { address, status } = useAppKitAccount();
+
+    const { } = useQuery({
+        queryKey: ["nonce", address],
+        enabled: !!address,
+        queryFn: async () => {
+            const res = await vinaswapApi.get(`/auth/nonce?address=${address}`);
+            return res.data.nonce as string;
+        },
+    });
 
     const isLoading = useMemo(() => {
         return status === "connecting";
