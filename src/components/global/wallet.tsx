950ea4d2d85b0acd7e4ebe4c0f00d91371072db5
HoangDucBach <hoangbach0985@gmail.com>
2025-06-07 06:01:58 +0700

    chore(kyc): not do anyth

--- src/components/global/wallet.tsx
+++ src/components/global/wallet.tsx
@@ -3,15 +3,26 @@
 import { useAppKit, useAppKitAccount, useAppKitState } from "@reown/appkit/react";
 import { Button, ButtonProps } from "../ui/button";
 import { useMemo } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { vinaswapApi } from "@/services/axios";
 
 interface ConnectWalletButtonProps extends ButtonProps {
 }
 export const ConnectWalletButton: React.FC<ConnectWalletButtonProps> = (props) => {
     const { open, } = useAppKit();
-    const { status } = useAppKitAccount();
+    const { address, status } = useAppKitAccount();
+
+    const { data: nonce, isLoading: isNonceLoading } = useQuery({
+        queryKey: ["auth:get:nonce", address],
+        enabled: !!address,
+        queryFn: async () => {
+            const res = await vinaswapApi.get(`/auth/nonce?address=${address}`);
+            return res.data.nonce as string;
+        },
+    });
 
     const isLoading = useMemo(() => {
-        return status === "connecting";
+        return status === "connecting" || status === "reconnecting" || isNonceLoading;
     }, [status]);
 
     return (
@@ -24,7 +35,7 @@ export const ConnectWalletButton: React.FC<ConnectWalletButtonProps> = (props) =
             }}
             {...props}
         >
-            Kết nối
+            Connect Wallet
         </Button>
     );
 }
