fa35c9fce922c2ad927f207f8de64fb77718c056
HoangDucBach <hoangbach0985@gmail.com>
2025-06-16 07:36:44 +0700

    feat(pools): set up

---
+++ src/app/(dashboard)/(trade)/pools/[address]/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+        <div>
+            <h1>Trade Pool Page</h1>
+            <p>This is the trade pool page for a specific address.</p>
+            {/* You can add more components or content here as needed */}
+        </div>
+    );
+}
