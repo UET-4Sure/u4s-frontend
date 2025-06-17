f0c073130d8af19dd3aa7e6890a546b0cbbb8684
HoangDucBach <hoangbach0985@gmail.com>
2025-06-17 20:59:29 +0700

    feat(pool-create-position): add 1/2 process

--- src/app/(dashboard)/(pool)/positions/create/_components/CreatePositionForm.tsx
+++ src/app/(dashboard)/(pool)/positions/create/_components/CreatePositionForm.tsx
@@ -3,11 +3,13 @@
 import { Button } from '@/components/ui/button';
 import { SelectTokenDialog } from '@/components/widgets/components/SelectTokenDialog';
 import { Token } from '@/components/widgets/type';
-import { HStack, StackProps, Text, VStack } from '@chakra-ui/react';
-import { useState } from 'react';
+import { HStack, Input, StackProps, StepsTitle, Text, useSteps, VStack } from '@chakra-ui/react';
+import { useMemo, useState } from 'react';
 import { Controller, SubmitHandler, useForm } from 'react-hook-form';
-import { TOKEN_LIST } from '../config';
 import { motion } from 'framer-motion';
+import { useTokenList } from '@/hooks/data/useTokenList';
+import { StepsCompletedContent, StepsContent, StepsIndicator, StepsItem, StepsList, StepsNextTrigger, StepsRoot } from '@/components/ui/steps';
+import { SwapWidget } from '@/components/widgets/swap/Swap';
 
 const MotionVStack = motion.create(VStack);
 
@@ -21,87 +23,205 @@ interface CreatePositionFormProps extends StackProps {
 }
 export const CreatePositionForm: React.FC<CreatePositionFormProps> = ({ children, ...props }) => {
     const [showAdvanced, setShowAdvanced] = useState(false);
-    const [currentStep, setCurrentStep] = useState(1);
+    const steps = useSteps({
+        defaultStep: 1,
+        count: 2,
+    })
+    const { data: tokenList } = useTokenList();
 
     const { register, handleSubmit, control, watch, setValue, formState: { errors } } = useForm<CreatePositionFormValues>({
     });
 
-    const onSubmit = (data: CreatePositionFormValues) => {
+    const onSubmit = async (data: CreatePositionFormValues) => {
         console.log("Form submitted with data:", data);
+
+        steps.goToNextStep();
         // Handle form submission logic here
     }
 
+    const Step1 = useMemo(() => () => (
+        <MotionVStack
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            align={"start"}
+            w={"full"}
+        >
+            <FormFieldTitle
+                title="Tạo vị thế"
+                description="Chọn token và nhập thông tin để tạo vị thế mới"
+            />
+            <HStack align={"start"} w={"full"}>
+                <Controller
+                    name="fromToken"
+                    control={control}
+                    render={({ field }) => (
+                        <SelectTokenDialog
+                            title={field.value?.symbol}
+                            triggerProps={{
+                                bg: field.value ? "bg.subtle" : "",
+                                color: field.value ? "bg.inverted" : "",
+                            }}
+                            tokenList={tokenList || []}
+                            placeholder="Chọn token cung cấp"
+                            selectedToken={field.value}
+                            onSelectToken={(token) => {
+                                field.onChange(token);
+                            }}
+                        />
+                    )}
+                />
+                <Controller
+                    name="toToken"
+                    control={control}
+                    render={({ field }) => (
+                        <SelectTokenDialog
+                            title={field.value?.symbol}
+                            tokenList={tokenList || []}
+                            placeholder="Chọn token nhận"
+                            selectedToken={field.value}
+                            onSelectToken={(token) => {
+                                field.onChange(token);
+                            }}
+                            triggerProps={{
+                                bg: field.value ? "bg.subtle" : "",
+                                color: field.value ? "bg.inverted" : "",
+                            }}
+                        />
+                    )}
+                />
+            </HStack>
+            <VStack w={"full"} align={"start"}>
+                <FormFieldTitle
+                    title="Bậc phí"
+                    description="Chọn bậc phí cho vị thế của bạn. Bậc phí ảnh hưởng đến chi phí giao dịch và hiệu suất của vị thế."
+                />
+            </VStack>
+            <StepsNextTrigger asChild>
+                <Button
+                    disabled={!watch("fromToken") || !watch("toToken")}
+                    w={"full"}
+                >
+                    Tiếp tục
+                </Button>
+            </StepsNextTrigger>
+        </MotionVStack >
+    ), [tokenList]);
+
+    const Step2 = useMemo(() => () => (
+        <MotionVStack
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            align={"start"}
+            w={"full"}
+        >
+            <FormFieldTitle
+                title="Thông tin vị thế"
+                description="Nhập thông tin chi tiết cho vị thế của bạn."
+            />
+            <HStack w={"full"} align={"start"}>
+                <VStack align={"start"} w={"full"}>
+                    <Text fontSize="sm" color="fg.subtle">Slippage Tối đa</Text>
+                    <Input
+                        type="number"
+                        placeholder="Nhập slippage tối đa"
+                        {...register("slippage", {
+                            required: "Slippage là bắt buộc",
+                            min: {
+                                value: 0,
+                                message: "Slippage không thể âm"
+                            },
+                            max: {
+                                value: 100,
+                                message: "Slippage không thể lớn hơn 100%"
+                            }
+                        })}
+                        defaultValue="1"
+                        variant={"subtle"}
+                        step="0.01"
+                        min="0"
+                        max="100"
+                        onChange={(e) => {
+                            const value = e.target.value;
+                            if (value === "" || /^\d*\.?\d*$/.test(value)) {
+                                setValue("slippage", value);
+                            }
+                        }}
+                    />
+                    {errors.slippage && <Text color="red.500">{errors.slippage.message}</Text>}
+                </VStack>
+            </HStack>
+            <VStack>
+                <FormFieldTitle
+                    title='Nạp token'
+                    description='Bạn có thể nạp token vào ví để sử dụng cho giao dịch này.'
+                />
+                <SwapWidget
+                    defaultFromToken={watch("fromToken")}
+                    defaultToToken={watch("toToken")}
+                    swapButtonProps={{
+                        hidden: true,
+                    }}
+                />
+            </VStack>
+
+            <StepsNextTrigger asChild>
+                <Button
+                    w={"full"}
+                    colorPalette={"primary"}
+                    type="submit"
+                >
+                    Tạo vị thế
+                </Button>
+            </StepsNextTrigger>
+        </MotionVStack>
+    ), []);
+
+    const stepRenders = [
+        {
+            title: "Tạo vị thế",
+            description: "Chọn token và nhập thông tin để tạo vị thế mới",
+            content: <Step1 />
+        },
+        {
+            title: "Thông tin vị thế",
+            description: "Nhập thông tin chi tiết cho vị thế của bạn.",
+            content: <Step2 />
+        }
+    ]
+
     return (
         <form onSubmit={handleSubmit(onSubmit)} style={{ width: '100%' }}>
-            {
-                currentStep === 1 && (
-                    <MotionVStack
-                        initial={{ opacity: 0, y: 20 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        exit={{ opacity: 0, y: -20 }}
-                        align={"start"}
-                        w={"full"}
-                    >
-                        <FormFieldTitle
-                            title="Tạo vị thế"
-                            description="Chọn token và nhập thông tin để tạo vị thế mới"
-                        />
-                        <HStack align={"start"} w={"full"}>
-                            <Controller
-                                name="fromToken"
-                                control={control}
-                                render={({ field }) => (
-                                    <SelectTokenDialog
-                                        title={field.value?.symbol}
-                                        triggerProps={{
-                                            bg: field.value ? "bg.subtle" : "",
-                                            color: field.value ? "bg.inverted" : "",
-                                        }}
-                                        tokenList={TOKEN_LIST}
-                                        placeholder="Chọn token cung cấp"
-                                        selectedToken={field.value}
-                                        onSelectToken={(token) => {
-                                            field.onChange(token);
-                                        }}
-                                    />
-                                )}
-                            />
-                            <Controller
-                                name="toToken"
-                                control={control}
-                                render={({ field }) => (
-                                    <SelectTokenDialog
-                                        title={field.value?.symbol}
-                                        tokenList={TOKEN_LIST}
-                                        placeholder="Chọn token nhận"
-                                        selectedToken={field.value}
-                                        onSelectToken={(token) => {
-                                            field.onChange(token);
-                                        }}
-                                        triggerProps={{
-                                            bg: field.value ? "bg.subtle" : "",
-                                            color: field.value ? "bg.inverted" : "",
-                                        }}
-                                    />
-                                )}
-                            />
-                        </HStack>
-                        <VStack w={"full"} align={"start"}>
-                            <FormFieldTitle
-                                title="Bậc phí"
-                                description="Chọn bậc phí cho vị thế của bạn. Bậc phí ảnh hưởng đến chi phí giao dịch và hiệu suất của vị thế."
-                            />
-                        </VStack>
-                        <Button
-                            w={"full"}
-                            onClick={() => setCurrentStep(2)}
+            <StepsRoot
+                colorPalette={"bg"}
+                orientation={"vertical"}
+                defaultStep={0}
+                count={stepRenders.length}
+            >
+                <StepsList>
+                    {stepRenders.map((step, index) => (
+                        <StepsItem
+                            key={index}
+                            index={index}
+                            title={step.title}
+                            description={step.description}
                         >
-                            Tiếp tục
-                        </Button>
-                    </MotionVStack>
-                )
-            }
-
+                            <StepsTitle>
+                                {step.title}
+                            </StepsTitle>
+                        </StepsItem>
+                    ))}
+                </StepsList>
+                {stepRenders.map((step, index) => (
+                    <StepsContent
+                        key={index}
+                        index={index}
+                    >
+                        {step.content}
+                    </StepsContent>
+                ))}
+            </StepsRoot>
         </form >
     );
 };
