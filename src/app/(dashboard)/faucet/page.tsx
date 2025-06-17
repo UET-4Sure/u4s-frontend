076f4a81418e506bb04f897779c1722ceed459c7
HoangDucBach <hoangbach0985@gmail.com>
2025-06-18 06:45:05 +0700

    fix(faucet): fix layout

--- src/app/(dashboard)/faucet/page.tsx
+++ src/app/(dashboard)/faucet/page.tsx
@@ -6,8 +6,8 @@ import { FaucetWidget } from "@/components/widgets/faucet/Faucet";
 export default function FaucetPage() {
     return (
         <Container maxW="container.sm" py={8}>
-            <VStack gap={6} align="stretch">
-                <Heading size="lg" textAlign="center">Token Faucet</Heading>
+            <VStack gap={6}>
+                <Heading size="4xl" textAlign="center">Token Faucet</Heading>
                 <FaucetWidget />
             </VStack>
         </Container>
