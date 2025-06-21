d973b0c1ace034e55d00b40ff47f8d00c8ecb9bf
HoangDucBach <hoangbach0985@gmail.com>
2025-06-21 07:52:57 +0700

    feat(landing): add motion for cover

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,9 +1,11 @@
 import { Center, Text } from "@chakra-ui/react";
 import { HeroSection } from "./HeroSection";
+import { Cover } from "./_components/Cover";
 
 export default function Home() {
   return (
     <Center>
+      <Cover/>
       <HeroSection />
     </Center>
   );
