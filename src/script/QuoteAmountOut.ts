bd04c1cd5df50ccd907de80b5b04a13ce64686bb
Drizzle210 <anhduc5a1tp@gmail.com>
2025-06-25 22:51:04 +0700

    fix: use amm price case high volume

--- src/script/QuoteAmountOut.ts
+++ src/script/QuoteAmountOut.ts
@@ -5,12 +5,30 @@ import { SwapExactInSingle } from '@uniswap/v4-sdk'
 import { getPoolConfig, getZeroForOne, TOKEN_ADDRESSES } from '@/app/(dashboard)/(trade)/swap/config'
 import { ethers } from 'ethers'
 import QUOTER_ABI from '@/abis/V4Quoter.json'
+import { queryOraclePrice } from './QueryOraclePrice';
+import { queryPoolInfo } from './QuerySqrtPrice';
 
 const RPC_URL = process.env.NEXT_PUBLIC_RPC_URL || "";
 
 const QUOTER_CONTRACT_ADDRESS = "0x61b3f2011a92d183c7dbadbda940a7555ccf9227";
 
 export async function quoteAmountOut(tokenIn: string, tokenOut: string, amountIn: number) {
+    
+    // NOTE: this is just a temporary solution to get the amount out for high volume trades
+    const oraclePrice = await queryOraclePrice(tokenIn);
+    const volume = amountIn * Number(oraclePrice);
+    if(amountIn <= 0 || volume > 10000) {
+        return 0;
+    }
+    if(volume >= 500) {
+        const poolInfo = await queryPoolInfo(tokenIn, tokenOut);
+        const sqrtPriceX96 = poolInfo.sqrtPriceX96;
+        
+        // Convert sqrtPriceX96 to actual price
+        const price = (Number(sqrtPriceX96) * Number(sqrtPriceX96)) / (2 ** 192);
+        return amountIn * price;
+    }
+    
     const quoterContract = new ethers.Contract(
         QUOTER_CONTRACT_ADDRESS,
         QUOTER_ABI.abi,
