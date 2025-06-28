3b485293acf91ff2868e3383f161776827f24cbc
Drizzle210 <anhduc5a1tp@gmail.com>
2025-06-28 11:15:04 +0700

    feat: update pool config

---
+++ src/app/(dashboard)/(trade)/swap/poolConfig.ts
@@ -0,0 +1,51 @@
+import { HOOK_CONTRACT_ADDRESS, TOKEN_ADDRESSES } from "./constants";
+
+export interface PoolConfig {
+  poolKey: {
+    currency0: string;
+    currency1: string;
+    fee: number;
+    tickSpacing: number;
+    hooks: string;
+  }
+}
+
+export const USDC_WETH_CONFIG: PoolConfig = {
+    poolKey: {
+      currency0: TOKEN_ADDRESSES.USDC.toLowerCase(),
+      currency1: TOKEN_ADDRESSES.WETH.toLowerCase(),
+      fee: 3000,
+      tickSpacing: 60,
+      hooks: HOOK_CONTRACT_ADDRESS.HOOK,
+    }
+};
+
+export const USDC_WBTC_CONFIG: PoolConfig = {
+  poolKey: {
+    currency0: TOKEN_ADDRESSES.USDC.toLowerCase(),
+    currency1: TOKEN_ADDRESSES.WBTC.toLowerCase(),
+    fee: 3000,
+    tickSpacing: 60,
+    hooks: HOOK_CONTRACT_ADDRESS.HOOK,
+  }
+};
+
+export const USDC_LINK_CONFIG: PoolConfig = {
+  poolKey: {
+    currency0: TOKEN_ADDRESSES.USDC.toLowerCase(),
+    currency1: TOKEN_ADDRESSES.LINK.toLowerCase(),
+    fee: 3000,
+    tickSpacing: 60,
+    hooks: HOOK_CONTRACT_ADDRESS.HOOK,
+  }
+};
+
+export const USDC_EUR_CONFIG: PoolConfig = {
+  poolKey: {
+    currency0: TOKEN_ADDRESSES.USDC.toLowerCase(),
+    currency1: TOKEN_ADDRESSES.EUR.toLowerCase(),
+    fee: 3000,
+    tickSpacing: 60,
+    hooks: HOOK_CONTRACT_ADDRESS.HOOK,
+  }
+};
