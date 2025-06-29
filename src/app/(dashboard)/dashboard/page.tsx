1890f8ce5517626b3feefaacce6a79d73c45eda6
HoangDucBach <hoangbach0985@gmail.com>
2025-06-29 19:28:39 +0700

    feat(trade): redirect trade to swap default

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,6 @@
 export default function Page() {
     return (
-        <div>
-            <h1>Dashboard</h1>
-            <p>Welcome to the dashboard!</p>
-        </div>
+        <>
+        </>
     );
 }
