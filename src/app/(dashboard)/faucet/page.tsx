11fe8562c0510b33ee20b7dc1a4d1943d4047233
HoangDucBach <hoangbach0985@gmail.com>
2025-06-30 20:30:48 +0700

    refactor(faucet): style and optimize

--- src/app/(dashboard)/faucet/page.tsx
+++ src/app/(dashboard)/faucet/page.tsx
@@ -1,13 +1,14 @@
-"use client";
-
-import { VStack, Container, Heading } from "@chakra-ui/react";
+import { VStack, Container, Heading, Text } from "@chakra-ui/react";
 import { FaucetWidget } from "@/components/widgets/faucet/Faucet";
 
 export default function FaucetPage() {
     return (
-        <Container maxW="container.sm" py={8}>
-            <VStack gap={6}>
-                <Heading size="4xl" textAlign="center">Token Faucet</Heading>
+        <Container maxW="container.sm" p={"8"}>
+            <VStack gap={"6"} w={"full"} align="start" h={"full"}>
+                <VStack align="stretch">
+                    <Heading size="4xl">Token Faucet</Heading>
+                    <Text color={"fg.subtle"}>Nhấp vào bất kỳ token nào để mint token vào ví của bạn</Text>
+                </VStack>
                 <FaucetWidget />
             </VStack>
         </Container>
